9b4c189594a1bf3be6bc045954444c1156b68f61
KRiabchenko <katerina.riabchenko90@gmail.com>
2022-09-23 14:54:01 +0300

    Add Task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+'use strict';
+import throttle from 'lodash.throttle'
+
+const LOCAL_STORAGE_KEY = 'feedback';
+const formRef = document.querySelector('.feedback-form');
+
+import { save, load, remove }  from './storage'
+
+initPage();
+
+const onFormSub = event => {
+    const { name, value } = event.target
+    let savedMessage = load(LOCAL_STORAGE_KEY);
+        
+    savedMessage = savedMessage ? savedMessage : {};
+     
+    savedMessage[name] = value;
+
+    save(LOCAL_STORAGE_KEY, savedMessage)
+
+}
+const throttleOnFormSub = throttle(onFormSub, 500);
+formRef.addEventListener('input', throttleOnFormSub);
+
+function initPage() {
+    const savedMessage = load(LOCAL_STORAGE_KEY);
+
+    if (!savedMessage) {
+        return;
+    }
+    Object.entries(savedMessage).forEach(([name, value]) => {
+        formRef.elements[name].value = value;
+    })
+}
+        
+
+const handelSub = event => {
+    event.preventDefault();
+    const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  console.log({email: email.value, message: message.value });
+  event.currentTarget.reset();
+  remove(LOCAL_STORAGE_KEY);
+}
+
+formRef.addEventListener('submit', handelSub);
 
