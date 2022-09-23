e98bd576993266a11cda8f9596080289a7447e08
KRiabchenko <katerina.riabchenko90@gmail.com>
2022-09-23 16:30:58 +0300

    Add Task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+// const player = require("@vimeo/player");
+import throttle from 'lodash.throttle'
+
+const VIDEOPLAYER_CURRENT_TIME = 'timeupdate';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
 
